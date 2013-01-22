ef087cfffa7c7075af65797a4b7aa6ef8facabce
sephiroth99 <sephiroth99@gmail.com>
2013-01-21 22:04:52 -0500

    Reduce size of Program.cs by splitting code in two other files

---
+++ translatr/Applicator.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System.Xml.XPath;
+
+namespace translatr
+{
+    class Applicator
+    {
+        public static void doApply(String transFilePath, String ovrFileBasePath, String ovrFilePatchPath)
+        {
+            LocalsFile localsFile = null;
+            List<CineFile> cineFileList = null;
+
+            String patchPathBase, origPatchPathBase;
+            String bigPathBase, origBigPathBase;
+            String outPath = "newpatch";
+            String dest;
+
+            Directory.CreateDirectory(outPath);
+
+            origBigPathBase = String.Empty;
+            origPatchPathBase = String.Empty;
+
+            // Get info from translations.xml file
+            System.Console.Write("Loading translation data...");
+            TransFile.Open(transFilePath, out localsFile, out cineFileList, out bigPathBase, out patchPathBase);
+            System.Console.WriteLine("done!");
+
+            if (ovrFileBasePath != String.Empty)
+            {
+                origBigPathBase = bigPathBase;
+                bigPathBase = ovrFileBasePath;
+
+                if (patchPathBase != String.Empty)
+                {
+                    if (ovrFilePatchPath == String.Empty)
+                    {
+                        System.Console.WriteLine("Error: A patch was specified in the translation file\nPatch override path must be provided!");
+                        System.Environment.Exit(-1);
+                    }
+                    else
+                    {
+                        origPatchPathBase = patchPathBase;
+                        patchPathBase = ovrFilePatchPath;
+                    }
+                }
+                System.Console.WriteLine("Using different source paths");
+                System.Console.WriteLine("Base: {0}", bigPathBase);
+                if (patchPathBase != String.Empty)
+                    System.Console.WriteLine("Patch: {0}", patchPathBase);
+                else
+                    System.Console.WriteLine("");
+            }
+
+            // Copy existing patch into output dir
+            System.Console.Write("Copying existing patch data...");
+            if (patchPathBase != "")
+            {
+                foreach (string filename in Directory.GetFiles(patchPathBase, "*.*", SearchOption.AllDirectories))
+                {
+                    dest = outPath + filename.Substring(patchPathBase.Length);
+                    Directory.CreateDirectory(Path.GetDirectoryName(dest));
+                    File.Copy(filename, dest, true);
+                }
+                System.Console.WriteLine("done!");
+            }
+            else
+            {
+                System.Console.WriteLine("No patch specified, skipping!");
+
+                //Creating empty bigfile.xml
+                var xmlsettings = new XmlWriterSettings();
+                xmlsettings.Indent = true;
+                XmlWriter xml = XmlWriter.Create(outPath + "\\bigfile.xml", xmlsettings);
+
+                // Open main files bigfile.xml to get values
+                var doc = new XPathDocument(bigPathBase + "\\bigfile.xml");
+                var nav = doc.CreateNavigator();
+                var root = nav.SelectSingleNode("/files");
+
+                xml.WriteStartDocument();
+                xml.WriteStartElement("files");
+                xml.WriteAttributeString("alignment", root.GetAttribute("alignment", ""));
+                xml.WriteAttributeString("endian", root.GetAttribute("endian", ""));
+                xml.WriteEndElement();
+                xml.WriteEndDocument();
+                xml.Flush();
+                xml.Close();
+            }
+
+            XmlDocument xdoc = new XmlDocument();
+            xdoc.Load(outPath + "\\bigfile.xml");
+
+            // Create new files in out dir
+            System.Console.WriteLine("Creating translated files...");
+
+            // locals.bin
+            if (localsFile != null)
+            {
+                dest = outPath + localsFile.name;
+                Directory.CreateDirectory(Path.GetDirectoryName(dest));
+
+                if (localsFile.sourcePath == origBigPathBase)
+                    localsFile.sourcePath = bigPathBase;
+                else if (localsFile.sourcePath == origPatchPathBase)
+                    localsFile.sourcePath = patchPathBase;
+
+                localsFile.rebuildAndSave(dest);
+                System.Console.WriteLine(dest);
+
+                if (localsFile.sourcePath.StartsWith(bigPathBase))
+                {
+                    // Create new entry node
+                    XmlNode n = xdoc.CreateNode(XmlNodeType.Element, "entry", null);
+                    n.InnerText = localsFile.name.Substring(1);
+
+                    // Add hash attribute
+                    var hashXmlAttr = xdoc.CreateAttribute("hash");
+                    hashXmlAttr.InnerText = hasher(Encoding.ASCII.GetBytes(localsFile.name.Substring(10).ToCharArray())).ToString("X").ToUpper();
+                    n.Attributes.Append(hashXmlAttr);
+
+                    // Add locale attribute
+                    var localeXmlAttr = xdoc.CreateAttribute("locale");
+                    localeXmlAttr.InnerText = localsFile.name.Substring(1, 8);
+                    n.Attributes.Append(localeXmlAttr);
+
+                    // Add node in file
+                    xdoc.DocumentElement.AppendChild(n);
+                }
+            }
+
+            // mul files
+            if (cineFileList.Count > 0)
+            {
+                foreach (CineFile cinefile in cineFileList)
+                {
+                    dest = outPath + cinefile.name;
+                    Directory.CreateDirectory(Path.GetDirectoryName(dest));
+
+                    // Replace the source path for the override one
+                    // If no override, orig*PathBase will be empty
+                    if (cinefile.sourcePath == origBigPathBase)
+                        cinefile.sourcePath = bigPathBase;
+                    else if (cinefile.sourcePath == origPatchPathBase)
+                        cinefile.sourcePath = patchPathBase;
+
+                    cinefile.rebuild(dest);
+                    System.Console.WriteLine(dest);
+
+                    if (cinefile.sourcePath.StartsWith(bigPathBase))
+                    {
+                        // Create new entry node
+                        XmlNode n = xdoc.CreateNode(XmlNodeType.Element, "entry", null);
+                        n.InnerText = cinefile.name.Substring(1);
+
+                        // Add hash attribute
+                        var hashXmlAttr = xdoc.CreateAttribute("hash");
+                        hashXmlAttr.InnerText = hasher(Encoding.ASCII.GetBytes(cinefile.name.Substring(10).ToCharArray())).ToString("X").ToUpper();
+                        n.Attributes.Append(hashXmlAttr);
+
+                        // Add locale attribute
+                        var localeXmlAttr = xdoc.CreateAttribute("locale");
+                        localeXmlAttr.InnerText = cinefile.name.Substring(1, 8);
+                        n.Attributes.Append(localeXmlAttr);
+
+                        // Add node in file
+                        xdoc.DocumentElement.AppendChild(n);
+                    }
+                }
+            }
+
+            System.Console.WriteLine("done!");
+
+            // Add new files in patch dir to bigfile.xml
+            System.Console.Write("Adding new files to \"bigfile.xml\"...");
+            xdoc.Save(outPath + "\\bigfile.xml");
+            System.Console.WriteLine("done!");
+        }
+        
+        private static Int32 hasher(byte[] s)
+        {
+            Int32 hash = -1;
+            const Int32 poly = 0x04C11DB7;
+
+            if (s.Length <= 0)
+                return hash;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                hash ^= (s[i] << 0x18);
+
+                for (int iter = 0; iter < 8; iter++)
+                {
+                    if (hash >= 0)
+                    {
+                        hash += hash;
+                    }
+                    else
+                    {
+                        hash += hash;
+                        hash ^= poly;
+                    }
+                }
+            }
+
+            hash ^= -1;
+
+            return hash;
+        }
+    }
+}
---
+++ translatr/Extractor.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace translatr
+{
+    class Extractor
+    {
+        public static void doExtract(String bigfilePath, String patchPath, bool isBigEndian, int lang)
+        {
+            TransFile tf = new TransFile(bigfilePath, patchPath, isBigEndian, lang);
+
+            var files = getFilelist(bigfilePath, patchPath, lang);
+
+            System.Console.WriteLine("Searching following files for translatable text:");
+
+            int f = 0;
+            foreach (string file in files)
+            {
+                System.Console.WriteLine("{0} of {1} - {2}", ++f, files.Count, Path.GetFileName(file));
+
+                if (file.EndsWith("locals.bin"))
+                {
+                    LocalsFile lf = new LocalsFile(isBigEndian);
+                    lf.parse(file);
+
+                    string basep;
+                    string name;
+                    if (file.StartsWith(bigfilePath))
+                    {
+                        basep = bigfilePath;
+                        name = file.Substring(bigfilePath.Length);
+                    }
+                    else
+                    {
+                        basep = patchPath;
+                        name = file.Substring(patchPath.Length);
+                    }
+                    tf.AddFile(basep, name);
+
+                    foreach (LocalsEntry e in lf.entries)
+                    {
+                        // Watch out not the same info as the attribute name!
+                        if (e.text != string.Empty)
+                            tf.AddEntry(e.text, e.index.ToString(), e.offset.ToString());
+                    }
+                }
+                else
+                {
+                    CineFile cf = new CineFile(isBigEndian);
+                    cf.parse(file);
+
+                    if (cf.isSubs())
+                    {
+                        string basep;
+                        string name;
+                        if (file.StartsWith(bigfilePath))
+                        {
+                            basep = bigfilePath;
+                            name = file.Substring(bigfilePath.Length);
+                        }
+                        else
+                        {
+                            basep = patchPath;
+                            name = file.Substring(patchPath.Length);
+                        }
+                        tf.AddFile(basep, name);
+
+                        List<SubtitleEntry> entries = cf.getSubtitles();
+
+                        foreach (SubtitleEntry e in entries)
+                        {
+                            if (e.lang == (LangID)lang)
+                            {
+                                tf.AddEntry(e.text, e.lang.ToString(), e.blockNumber.ToString());
+                            }
+                        }
+                    }
+                }
+            }
+
+            tf.Close();
+            System.Console.WriteLine("Translatable text saved to file \"translations.xml\"");
+        }
+
+        private static List<String> getFilelist(String bigfilePath, String patchPath, int lang)
+        {
+            List<String> patchedfiles = new List<String>();
+            if (patchPath != String.Empty)
+            {
+                patchedfiles = searchDir(patchPath, lang);
+            }
+
+            var bigfiles = searchDir(bigfilePath, lang);
+
+            // Replace patched files in main file list
+            foreach (string s in patchedfiles)
+            {
+                //bigfiles.RemoveAll(delegate (string search) { return search == (bigfilePath + s.Remove(0, patchPath.Length)); });
+                bigfiles.RemoveAll(delegate(string search) { return Path.GetFileName(search) == Path.GetFileName(s); });
+                bigfiles.Add(s);
+            }
+
+            return bigfiles;
+        }
+
+        private static List<String> searchDir(String path, int lang)
+        {
+            List<String> list = new List<String>();
+
+            var dirlist = Directory.GetDirectories(path);
+
+            foreach (string d in dirlist)
+            {
+                string subfolder = d.Substring(d.LastIndexOf("\\") + 1);
+
+                if (subfolder == "default")
+                {
+                    //We may have a locals.bin in the default folder (ex. russian version)
+                    //In that case there shouldn't be one in the locale folder
+                    var localsfiles = Directory.GetFiles(d, "locals.bin", SearchOption.AllDirectories);
+                    if (localsfiles.Length > 0)
+                    {
+                        list.AddRange(localsfiles); // Should only have one!
+                    }
+
+                    var files = Directory.GetFiles(d, "*.mul", SearchOption.AllDirectories);
+                    if (files.Length > 0)
+                    {
+                        list.AddRange(files);
+                    }
+                }
+                else
+                {
+                    int folderLocale = Int32.Parse(subfolder, System.Globalization.NumberStyles.HexNumber);
+                    int mask = getLocaleMask(lang);
+                    int locale = (1 << lang);
+
+                    if (((folderLocale & mask) & locale) != 0)
+                    {
+                        // Get mul files
+                        var files = Directory.GetFiles(d, "*.mul", SearchOption.AllDirectories);
+                        if (files.Length > 0)
+                        {
+                            list.AddRange(files);
+                        }
+
+                        var localsfiles = Directory.GetFiles(d, "locals.bin", SearchOption.AllDirectories);
+                        if (localsfiles.Length > 0)
+                        {
+                            list.AddRange(localsfiles); // Should only have one!
+                        }
+                    }
+                }
+            }
+
+            return list;
+        }
+
+        /*private static LangID getLocale(Int32 code)
+        {
+            if (code == (Int32)LangID.Default)
+            {
+                return LangID.Default;
+            }
+
+            int i = 0;
+            while ((code & (Int32)(1 << i)) == 0)
+                i++;
+
+            return (LangID)i;
+        }*/
+
+        private static int getLocaleMask(int lang)
+        {
+            LangID l = (LangID)lang;
+
+            if (l == LangID.Russian)
+                return 0x0200;
+            else if (l == LangID.Japanese)
+                return 0x0020;
+            else if (l == LangID.Polish || l == LangID.Czech || l == LangID.Hungarian)
+                return 0x1480;
+            else if (l == LangID.English || l == LangID.French || l == LangID.Italian || l == LangID.German || l == LangID.Spanish || l == LangID.Dutch)
+                return 0x081F;
+            else
+                return 0;
+        }
+    }
+}
